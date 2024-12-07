45953a4eee5a96866b06437b7bc125e05a13aa35
Muhammad Raehan Putra H <128665717+amrptraaa@users.noreply.github.com>
2024-12-07 13:46:25 +0700

    rehan_training1

---
+++ src/page/Portofolio.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Portofolio() {
+    return (
+        <>
+            <div>
+                <h1>Welcome to the Portofolio Page</h1>
+            </div>    <div>
+                <h1>Welcome to the Portofolio Page</h1>
+            </div>
+        </>
+
+    );
+}
+
+export default Portofolio;
